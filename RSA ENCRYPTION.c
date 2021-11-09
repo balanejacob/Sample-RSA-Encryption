f9c6a4424e6298c803b29690fa525ae75f412a0e
jacob <balanejacob1@gmail.com>
2021-11-09 23:02:11 +0800

    Update RSA ENCRYPTION.c

--- RSA ENCRYPTION.c
+++ RSA ENCRYPTION.c
@@ -3,10 +3,36 @@
 #include <string.h>
 #include <math.h>
 
+void decrypt(int p,int q,char string[]){
+	int n,z,e,d,i,length;
+	double m,c;
+	char decrypted[100]="";
+	
+	printf("\n\nEncrypted Text: \n >> %s",string);
+	
+	length=strlen(string);
+	
+	n=p*q;
+	e=7;
+	z=(p-1)*(q-1);
+	d=10;
+	while(((d*e)-1)%z!=0){
+		d++;
+	}
+	
+	printf("\nPlain Text: \n >> ");
+	
+	for(i=0;i<length;i++){
+		c=string[i];
+		m=pow(c,d);
+		m=fmod(m,n);
+		decrypted[i]=(int)m;
+	}
+	printf("%s\n",decrypted);
+}
 
-
-void display_c(double a){
-	printf("%d",(int)a);
+void display_c(char a){
+	printf("%c",a);
 }
 
 int gcd(int a, int b){
@@ -22,8 +48,9 @@ int gcd(int a, int b){
 
 
 void rsa_encryption(int p,int q,char string[]){
-	int n,z,e,d=1,i,length;
+	int n,z,e,d,i,length;
 	double m,c;
+	char encrypted[100]="";
 	
 	printf("\n\nPlain Text: \n >> %s",string);
 	
@@ -52,14 +79,16 @@ void rsa_encryption(int p,int q,char string[]){
 		m=string[i];
 		c=pow(m,e);
 		c=fmod(c,n);
-		display_c(c);
+		encrypted[i]=(char)c;
 	}
+	printf("%s\n",encrypted);
 	
+	decrypt(p,q,encrypted);
 }
 
 int main(){
-	rsa_encryption(5,7,"encrypt");
-	rsa_encryption(3,5,"secure");
+	rsa_encryption(11,13,"ENCRYPTION");
+	rsa_encryption(11,13,"RASTAMAN");
 	return 0;
 }
 
